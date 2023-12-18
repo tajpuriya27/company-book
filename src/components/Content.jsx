a53ccabd427772fcdb28e1bdbd3ce5822f8176b8
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-18 12:12:23 +0545

    feat: aaded search input

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,34 @@
 import companies from "../data/companies.json";
 import Card from "./Card";
-console.log("companies", companies);
+import { useState } from "react";
+
+// console.log("companies", companies);
 const Content = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchedCompanies = companies.filter((val) => {
+    if (searchTerm === "") {
+      return val;
+    } else if (
+      val.company_name.toLowerCase().includes(searchTerm.toLowerCase())
+    ) {
+      return val;
+    }
+  });
   return (
     <div className="content-container">
       <h1>Companies</h1>
+      <input
+        type="text"
+        placeholder="Search First Name....."
+        autoFocus
+        value={searchTerm}
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
       <div className="companies-container">
-        {companies.map((company) => (
+        {searchedCompanies.map((company) => (
           <Card key={company.id} {...company} />
         ))}
       </div>
